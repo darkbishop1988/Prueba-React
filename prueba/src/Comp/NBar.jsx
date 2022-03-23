b7d3efe543041466c9c6b1575825cb87b847f633
Gonzalez Emiliano <emilianoalbertogonzalez@gmail.com>
2022-03-23 17:43:42 -0300

    adding NBAR

---
+++ prueba/src/Comp/NBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function NBar () { 
+
+return (
+ 
+<> <nav id="Container">
+ <div className="Home" > Home</div>
+ <div className="Contact" > Info</div>
+ <div className="Products">Contact</div>
+ <div className="Products">Gallery</div></nav>
+
+
+</>
+
+
+)
+
+}
